2b46860bf5ad00cb908a01b7a67ed0092d00f0f1
Leonardo Tosin <LToso>
2022-09-01 18:44:25 -0300

    fixing

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
 app.use("/user", userRoute);
 app.use("/test", testRoute);
 app.use("/usertest", userTestRoute);
-app.use("/userPremium", userPremiumRoute);
+app.use("/userpremium", userPremiumRoute);
 
 //Erro API
 app.use((req, res, next) => {
